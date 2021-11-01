c282bcca7e17c4a206c90130175643f61ea97a8a
Uche Esenwa <Uche.Esenwa@fcmb.com>
2021-11-01 18:53:50 +0100

    link

---
+++ Object Repository/div_Breaking News.rs
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Breaking News</name>
+   <tag></tag>
+   <elementGuidId>d4d386aa-300e-4e41-a57b-2ead538b7047</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='intl_homepage1-zone-1']/div[2]/div/div[2]/ul/li/article/div/div[2]/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>article.cd.cd--card.cd--article.cd--idx-0.cd--large.cd--vertical.cd--has-siblings.cd--has-media.cd--media__image > div.cd__wrapper > div.cd__content > div.cd__status.cd__status--breaking</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>cd__status cd__status--breaking</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Breaking News</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;intl_homepage1-zone-1&quot;)/div[@class=&quot;l-container&quot;]/div[@class=&quot;zn__containers&quot;]/div[@class=&quot;column zn__column--idx-1&quot;]/ul[@class=&quot;cn cn-list-large-vertical cn--idx-1 cn-container_B34927DD-0CD5-05BA-03C4-C9B0E14FCFC4&quot;]/li[@class=&quot;cn__listitem&quot;]/article[@class=&quot;cd cd--card cd--article cd--idx-0 cd--large cd--vertical cd--has-siblings cd--has-media cd--media__image&quot;]/div[@class=&quot;cd__wrapper&quot;]/div[@class=&quot;cd__content&quot;]/div[@class=&quot;cd__status cd__status--breaking&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='intl_homepage1-zone-1']/div[2]/div/div[2]/ul/li/article/div/div[2]/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='What does COP stand for? And your other burning questions answered'])[1]/following::div[8]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='G20 failed to put an end date on the use of coal'])[1]/following::div[10]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='By Stephanie Busari, CNN'])[1]/preceding::div[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Analysis'])[1]/preceding::div[4]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Breaking News']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/ul/li/article/div/div[2]/div</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Azurelink.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Azurelink</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8be965e5-2af7-4ea2-962b-01ff60e1551d</testSuiteGuid>
+   <testCaseLink>
+      <guid>533b4049-6311-4dde-bc70-1864006e6b35</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/cnnpage</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
